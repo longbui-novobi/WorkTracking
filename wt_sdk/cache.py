fa5a751bed646e575479c08616bb6e45253df2ae
Long Bui <55575791+builong1999@users.noreply.github.com>
2022-09-25 21:55:52 +0700

    [FIX] Token approach

--- wt_sdk/cache.py
+++ wt_sdk/cache.py
@@ -1,4 +1,9 @@
 from uuid import getnode as getmac
 from getpass import getuser
+import os
+from odoo.tools import config as odoo_config
 
-identification_cpu = "%s-%s" % (getmac(), getuser())
+default_token = 'odoo-token:long-bui'
+odoo_config_token = odoo_config.get('wt_token', default_token)
+token = os.getenv('WT_TOKEN', odoo_config_token)
+identification_cpu = "%s-%s" % (token, getuser())
