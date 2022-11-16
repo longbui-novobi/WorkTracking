a72f5493e4381f00bcc3dcb4c9a327ff12b69b45
builong1999 <philonglml9@gmail.com>
2022-11-16 21:27:47 +0700

    REL

--- wt_migration/models/res_users.py
+++ wt_migration/models/res_users.py
@@ -34,7 +34,7 @@ class ResUsers(models.Model):
             to_fetch_projects and to_fetch_projects.sudo().load_new_project()
         else:
             raise UserError(_("The Token is invalid, please check again"))
-        self.env['ir.config_parameter'].set_param('latest_unix', int(datetime.now().timestamp() * 1000))
+        self.env['ir.config_parameter'].sudo().set_param('latest_unix', int(datetime.now().timestamp() * 1000))
 
     def token_exists(self):
         existing_token_users = self.env['res.users']
