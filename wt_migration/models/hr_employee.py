e1e96e7eedb567c32aad595982db342bea4bf84b
builong1999 <philonglml9@gmail.com>
2023-02-08 23:26:02 +0700

    REL

--- wt_migration/models/hr_employee.py
+++ wt_migration/models/hr_employee.py
@@ -6,7 +6,6 @@ class HREmployee(models.AbstractModel):
 
     is_wt_admin = fields.Boolean(string="Admin?", tracking=True)
     auto_export_work_log = fields.Boolean(string='Auto Export Logs', tracking=True)
-    auto_remove_access = fields.Boolean(string="Auto Remove Access", default=True)
 
     def action_reset_extension_token(self):
         self.ensure_one()
