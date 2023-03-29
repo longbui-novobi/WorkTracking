c3eab03924eeb3c357b9676f97cb1be1abba45fe
builong1999 <philonglml9@gmail.com>
2023-03-29 11:32:40 +0700

    REL

--- wt_migration/models/wt_overall.py
+++ wt_migration/models/wt_overall.py
@@ -88,7 +88,6 @@ class WtTimeLog(models.Model):
                             _logger.error(e)
             exported_values = {**values, **{'export_state': 1}}
             if processed_records:
-                _logger.info('processed_records')
                 super(WtTimeLog, processed_records.with_context(bypass_exporting_check=True)).write(exported_values)
             exported_logs = (self-processed_records).filtered(lambda r: r.export_state >= 1)
             if exported_logs:
@@ -98,11 +97,9 @@ class WtTimeLog(models.Model):
                     log_by_state[state] |= log
                 for state, logs in log_by_state.items():
                     exported_values['export_state'] = state
-                    _logger.info('exported_values %s' % exported_values)
                     super(WtTimeLog, logs.with_context(bypass_exporting_check=True)).write(exported_values)
             to_update_records -= (processed_records | exported_logs)
         if to_update_records:
-            _logger.info('to_update_records')
             res = super(WtTimeLog, to_update_records).write(values)
         return res
 
